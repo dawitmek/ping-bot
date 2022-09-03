42ea8b87543a6eb47640c3729b9a619389052b82
Dawit <dawitmekuria5@gmail.com>
2022-09-03 03:12:19 -0400

    second commit ysers fubd

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.json');
+const token = process.env.PINGBOTTOKEN;
+const clientId = process.env.PingClient;
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Pings users that you are in the call. PRESS ENTER FIRST.'),
-	new SlashCommandBuilder().setName('ping-edit').setDescription('Edits collection of users'),
+	new SlashCommandBuilder().setName('ping-edit').setDescription('Edits collection of users')
+	.addStringOption(option => 
+		option.setName('add-users')
+		.setDescription('Add/Change users to ping. Add @\'s')
+		.setRequired(true)),
 	new SlashCommandBuilder().setName('ping-help').setDescription('Know all the commands of the bot!'),
 	new SlashCommandBuilder().setName('ping-message').setDescription('Update the message you want to send!'),
+
 
 
 ]
--- index.js
+++ index.js
@@ -1,115 +1,185 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
-const { log } = require('node:console');
+const {
+    Client,
+    Collection,
+    Intents
+} = require('discord.js');
+const {
+    log
+} = require('node:console');
 
-const rawObj = fs.readFileSync('./storage.json');
-const objStorage = JSON.parse(rawObj);
+const {
+    MongoClient
+} = require('mongodb');
+const {
+    LOADIPHLPAPI
+} = require('node:dns');
+
+const token = process.env.PINGBOTTOKEN;
+
+const dbClient = new MongoClient(process.env.MongoClient);
+
 
 // Create a new client instance Discord.js
-const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+const client = new Client({
+    intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]
+});
 
 client.commands = new Collection();
 
 // When the client is ready, run this code (only once) Discord.js
 client.once('ready', () => {
     console.log('Ready from discord.js!');
-    client.user.setActivity("Your mother", { type: "PLAYING" });
+    client.user.setActivity("Friends", {
+        type: "WATCHING"
+    });
 });
 
 
 client.on("messageCreate", (message) => {
     if (message.author.bot) return false;
-    // '633454718005280788'.send('brr');
-    // const user = await client.users.fetch('id');
-    // user.send('content');
-
 });
+
+async function findUsers(guild, user) {
+    try {
+        return await dbClient.db('Ping-Bot').collection(guild).findOne({
+            id: user
+        });
+    } catch (error) {
+        console.error(error);
+        // throw(new Error('Couldn\'t find users'));
+    }
+}
+
+async function updateUsers(guild, user, newUsers) {
+    //**************************** Tried to check for bots here *******************
+
+    // newUsers.forEach(user => {
+    //     fetchUser(user)
+    //         .then((userCheck) => {
+    //             try {
+    //                 if (userCheck.bot) {
+    //                     throw new Error('BOTPINGERROR: Tried to ping a bot');
+    //                 }
+    //             } catch (error) {
+    //                 return error;
+    //             }
+    //         })
+    // })
+
+    //********************************************************************* */
+
+    // trys to find & replace existing ping users 
+    let replaced = await dbClient.db('Ping-Bot').collection(guild).replaceOne({
+        id: user
+    }, {
+        id: user,
+        pingUsers: newUsers
+    });
+    console.log(replaced);
+    // if replace returns error, creates document
+    if (!replaced.upsertedId) {
+        return await dbClient.db('Ping-Bot').collection(guild).insertOne({
+            id: user,
+            pingUsers: newUsers
+        });
+    } else
+        return replaced;
+}
+
+async function fetchUser(user) {
+    return await client.users.fetch(user);
+}
+async function databaseConnect() {
+    await dbClient.connect();
+}
+async function closeDatabase() {
+    await dbClient.close();
+}
 //	Discord.js
 client.on('interactionCreate', async interaction => {
+    //  ************************ connect to database ************************
+    try {
+        await databaseConnect();
+    } catch (error) {
+        interaction.reply({
+            content: "Error connecting to database. Try again later",
+            ephemeral: true
+        });
+    }
+
     if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
+    const {
+        commandName
+    } = interaction;
 
     if (commandName === 'ping') {
-        if (!objStorage[interaction.guildId]) {
-            objStorage[interaction.guildId] = {};
-        }
-
-        if (objStorage[interaction.guildId][interaction.user.id]) {
-            let usersArr = objStorage[interaction.guildId][interaction.user.id];
-            let channel = client.channels.cache.get(interaction.channelId);
-            usersArr.forEach(elem => {
-
-                new Promise((resolve) => {
-                    resolve(client.users.fetch(elem));
-                }).then((user) => {
-                    if (!user.bot) {
-                        user.send(`${interaction.user.username} is currently in the call!`);
-                    } else {
-                        channel.send("You can't ping a bot!");
-                        throw(new Error('Tryed to ping a bot'));
-                    }
-                }).then(() => {
-                    interaction.reply({ content: 'Sent!', ephemeral: true });
-                })
-                
-                .catch((error) => {
-                    console.log(error)
-                })
-
-            })
-        } else {
-            objStorage[interaction.guildId][interaction.user.id] = '';
-            interaction.reply({ content: 'Enter the @\'s you want to ping.', ephemeral: true }).then(() => {
-                const filter = m => interaction.user.id === m.author.id;
-
-                interaction.channel.awaitMessages({ filter, time: 60000, max: 1, errors: ['time'] })
-                    .then(messages => {
-                        let usersArr = messages.first().content.split('>').map(elem => {
-                            return elem.trim().slice(2);
-                        });
-                        usersArr.splice(-1, 1);
-                        objStorage[interaction.guildId][interaction.user.id] = usersArr;
-                        interaction.followUp({ content: 'Sent!', ephemeral: true });
-                        usersArr.forEach(elem => {
-
-                            new Promise((resolve) => {
-                                resolve(client.users.fetch(elem));
-                            }).then((user) => {
+        try {
+            let usersArr = await findUsers(interaction.guildId, interaction.user.id);
+            //iterates through all the users to be pinged
+            if (usersArr) {
+                usersArr.pingUsers.forEach(elem => {
+                    new Promise((resolve) => {
+                            // Finds their DM address
+                            resolve(client.users.fetch(elem));
+                        }).then((user) => {
+                            if (!user.bot) {
                                 user.send(`${interaction.user.username} is currently in the call!`);
-                            }).catch((error) => {
-                                interaction.reply({ content: `There was an error: ${error} `, ephemeral: true })
-                            })
+                            } else {
+                                interaction.reply({
+                                    content: "You can't ping a bot!",
+                                    ephemeral: true
+                                });
+                                throw (new Error('Tryed to ping a bot'));
+                            }
                         })
-                        fs.writeFileSync('./storage.json', JSON.stringify(objStorage, null, 2))
-                    }).catch(() => interaction.followUp('You didn\'t enter any input.'));
-            })
+                        .catch((error) => {
+                            console.error(error)
+                        })
+                        .then(() => {
+                            interaction.reply({
+                                content: 'Sent!',
+                                ephemeral: true
+                            });
+                        })
+
+                })
+            } else {
+                interaction.reply({
+                    content: 'No users to ping!. Create one using /ping-edit',
+                    ephemeral: true
+                })
+            }
+        } catch (error) {
+            interaction.reply({
+                content: 'Error Pinging. Try again later.' + error,
+                ephemeral: true
+            });
         }
     } else if (commandName === 'ping-edit') {
-        interaction.reply({ content: 'Enter the new usernames to ping.', ephemeral: true }).then(() => {
-            const filter = m => interaction.user.id === m.author.id;
+        let message = interaction.options._hoistedOptions[0].value;
+        let usersArr = message.split('>').map(elem => {
+            return elem.trim().slice(3);
+        });
+        usersArr.splice(-1, 1);
+        await updateUsers(interaction.guildId, interaction.user.id, usersArr)
+            .then(() => interaction.reply({
+                content: 'Updated users!',
+                ephemeral: true
+            }))
 
-            interaction.channel.awaitMessages({ filter, time: 60000, max: 1, errors: ['time'] })
-                .then(messages => {
-                    let usersArr = messages.first().content.split('>').map(elem => {
-                        return elem.trim().slice(2);
-                    });
-                    usersArr.splice(-1, 1);
 
-                    objStorage[interaction.guildId][interaction.user.id] = usersArr;
 
-                    fs.writeFileSync('./storage.json', JSON.stringify(objStorage, null, 2))
-                    interaction.followUp({ content: 'Edited!', ephemeral: true });
-                })
-        })
-    } else if(commandName === 'ping-help') {
+    } else if (commandName === 'ping-help') {
         const message = interaction.options.getString('message');
         interaction.reply(message);
     }
-});
 
+    // ************************ close database ************************
+    await closeDatabase();
+});
 
 
 // Login to Discord with your client's token
